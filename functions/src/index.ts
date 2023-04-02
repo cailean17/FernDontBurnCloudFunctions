272b821816a7a1512dcd1bf8cf79cc132a7bfac4
cailean17 <55571023+cailean17@users.noreply.github.com>
2023-04-01 21:17:32 -0700

    Cloud functions finished

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,54 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-admin.initializeApp(functions.config().firebase);
-
-const db = functions.database;
-
-// let excessiveTilt = false;
-// let fireDetected = false;
-// let batteryLow = false;
-
+admin.initializeApp();
 
 // compromised pole
-exports.checkTilt = db.ref("/San Jose Utility/{pole_id}/tilt")
+exports.alertCompromisedPole = functions.firestore.
+  document("/San Jose Utility Poles/{pole_id}")
   .onUpdate((event, eventContext) => {
+    let tiltChanged = false;
+    let batteryChanged = false;
+    let Co2Changed = false;
+
     const poleName = eventContext.params.pole_id;
 
-    // const previousValue: number = +event.before.val;
-    const newValue: number = +event.after.val;
+    const previousTilt: number = +event.before.get("tilt");
+    const newTilt: number = +event.after.get("tilt");
 
-    if (newValue >= 90) {
-      functions.logger.warn("EXTREME ALERT: Pole:" + poleName +
-      " has exceeded normal tilt parameter");
+    const previousBattery: number = +event.before.get("battery");
+    const newBattery: number = +event.after.get("battery");
+
+    const previousCo2: number = +event.before.get("Co2 concentration");
+    const newCo2: number = +event.after.get("Co2 concentration");
+
+    if (previousTilt != newTilt) {
+      tiltChanged = true;
+    }
+
+    if (previousBattery != newBattery ) {
+      batteryChanged = true;
+    }
+
+    if (previousCo2 != newCo2) {
+      Co2Changed = true;
+    }
+
+    if (tiltChanged) {
+      if (newTilt >= 90) {
+        functions.logger.warn("EXTREME ALERT: Pole:" + poleName +
+        " has exceeded normal tilt parameter");
+      }
+    }
+    if (batteryChanged) {
+      if (newBattery <= 50) {
+        functions.logger.warn("WARNING: " + poleName + " Battery Level:" +
+       newBattery);
+      }
+    }
+    if (Co2Changed) {
+      if ( newCo2 >= 0.5) {
+        functions.logger.warn("WARNING FIRE at " + poleName );
+      }
     }
   });
